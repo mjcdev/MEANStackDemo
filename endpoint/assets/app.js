3306a862ee0e49df513e88714c47059d01083fd0
Mike Childs <mchilds88@googlemail.com>
2015-09-22 21:19:58 +0100

    Modularising the Controller from the Service
    Pulling the common http calls out into a service, using the service from
    the controller

--- endpoint/assets/app.js
+++ endpoint/assets/app.js
@@ -1,21 +1,30 @@
 var app = angular.module('app', [])
-			app.controller('PostsCtrl', function($scope, $http) {
-				
-				$http.get('http://localhost:3000/api/posts')
-					.success(function(posts) {
-						$scope.posts = posts
-					})
-				
-				$scope.addPost = function () {					
-					if($scope.postBody)
-					{
-						$http.post('/api/posts', {
-							username: 'mjcdev',
-							body: $scop.postBody
-						}).success(function (post) {
-							$scope.posts.unshift(post)
-							$scope.postBody = null
-						})					
-					}
-				}
-			})			
+
+app.service('PostsSvc', function ($http) {
+	this.fetch = function () {
+		return $http.get('/api/posts')
+	}
+	this.create = function (post)
+	{
+		return $http.post('/api/posts', post)
+	}
+})
+
+app.controller('PostsCtrl', function($scope, PostsSvc) {		
+	$scope.addPost = function () {					
+		if($scope.postBody)
+		{
+			PostsSvc.create({
+				username: 'mjcdev',
+				body: $scope.postBody
+			}).success(function (post) {
+				$scope.posts.unshift(post)
+				$scope.postBody = null
+			})					
+		}
+	}
+	
+	PostsSvc.fetch().success(function (posts) {
+		$scope.posts = posts
+	})
+})
